2878717733bd4e042a50d2badbdb6b98f1d6fda4
Jannik Hegemann <jannikhegemann1207@gmail.com>
2022-10-09 22:38:07 +0200

    init webserver.java base

--- terium-cloud/src/main/java/cloud/terium/cloudsystem/webinterface/WebServer.java
+++ terium-cloud/src/main/java/cloud/terium/cloudsystem/webinterface/WebServer.java
@@ -1,4 +1,25 @@
 package cloud.terium.cloudsystem.webinterface;
 
+import cloud.terium.cloudsystem.Terium;
+import com.sun.net.httpserver.HttpServer;
+import lombok.SneakyThrows;
+
+import java.net.InetSocketAddress;
+
 public class WebServer {
+
+    private final HttpServer httpServer;
+
+    @SneakyThrows
+    public WebServer() {
+        this.httpServer = HttpServer.create(new InetSocketAddress("0.0.0.0", Terium.getTerium().getConfigManager().getInt("web_port")), 0);
+    }
+
+    private void startWebServer() {
+        this.httpServer.start();
+    }
+
+    public void stopWebServer() {
+        this.httpServer.stop(0);
+    }
 }
