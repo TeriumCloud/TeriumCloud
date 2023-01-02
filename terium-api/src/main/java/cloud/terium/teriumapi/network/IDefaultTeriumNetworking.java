b1dd2263e669125332c8e2ef668ac69a8ed0f28b
Jannik H <jannikhegemann1207@gmail.com>
2023-01-02 20:28:07 +0100

    added netty as implemention to terium-api for repo later

--- terium-api/src/main/java/cloud/terium/teriumapi/network/IDefaultTeriumNetworking.java
+++ terium-api/src/main/java/cloud/terium/teriumapi/network/IDefaultTeriumNetworking.java
@@ -1,6 +1,14 @@
 package cloud.terium.teriumapi.network;
 
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.nio.channels.Channel;
+
 public interface IDefaultTeriumNetworking {
+
+    Channel getChannel();
+
+    void addHandler(SimpleChannelInboundHandler<Packet> handler);
 
     void sendPacket(Packet packet);
 }
