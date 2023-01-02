6fb3ebf6594d88c3420d203b42db5ebecae9f020
Jannik H <jannikhegemann1207@gmail.com>
2023-01-02 20:15:19 +0100

    write documentation for ICommandFactory and Command in terium-api

--- terium-api/src/main/java/cloud/terium/teriumapi/console/command/Command.java
+++ terium-api/src/main/java/cloud/terium/teriumapi/console/command/Command.java
@@ -13,14 +13,32 @@ public abstract class Command implements Serializable {
     private final String description;
     private final String[] aliases;
 
+    /**
+     * Constructor to create a command.
+     *
+     * @param command String
+     * @param description String
+     * @param aliases String[]
+     */
     public Command(String command, String description, String... aliases) {
         this.command = command;
         this.description = description;
         this.aliases = aliases;
     }
 
+    /**
+     * What the command should execute.
+     *
+     * @param args
+     */
     public abstract void execute(String[] args);
 
+    /**
+     * Overwrite that methode to write your own tab completion for the command.
+     *
+     * @param args
+     * @return List<String>
+     */
     public List<String> tabComplete(String[] args) {
         return List.of();
     }
--- terium-api/src/main/java/cloud/terium/teriumapi/console/command/ICommandFactory.java
+++ terium-api/src/main/java/cloud/terium/teriumapi/console/command/ICommandFactory.java
@@ -2,5 +2,10 @@ package cloud.terium.teriumapi.console.command;
 
 public interface ICommandFactory {
 
+    /**
+     * Register a command.
+     *
+     * @param command
+     */
     void registerCommand(Command command);
 }
