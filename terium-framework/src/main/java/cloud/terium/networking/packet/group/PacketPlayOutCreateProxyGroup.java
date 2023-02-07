4853b85ee0ae5fcfe55dd7ecf40ef690f81b6529
Jannik H <jannikhegemann1207@gmail.com>
2023-02-07 20:55:01 +0100

    transform service group packets to new packet class structure

--- terium-framework/src/main/java/cloud/terium/networking/packet/group/PacketPlayOutCreateProxyGroup.java
+++ terium-framework/src/main/java/cloud/terium/networking/packet/group/PacketPlayOutCreateProxyGroup.java
@@ -1,160 +1,37 @@
 package cloud.terium.networking.packet.group;
 
+import cloud.terium.teriumapi.TeriumAPI;
 import cloud.terium.teriumapi.network.Packet;
 import cloud.terium.teriumapi.node.INode;
 import cloud.terium.teriumapi.template.ITemplate;
 
-import java.net.InetSocketAddress;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-public record PacketPlayOutCreateProxyGroup(String name, String groupTitle, INode node, List<INode> fallbackNodes,
-                                            List<ITemplate> templates,
+public record PacketPlayOutCreateProxyGroup(String name, String groupTitle, String node, List<String> fallbackNodes,
+                                            List<String> templates,
                                             String version, boolean maintenance, boolean isStatic, int port,
                                             int maximumPlayers,
                                             int memory, int minimalServices, int maximalServices) implements Packet {
 
-    @Override
-    public List<ITemplate> templates() {
-        List<ITemplate> templateList = new ArrayList<>();
-        templates.forEach(template -> {
-            templateList.add(new ITemplate() {
-                @Override
-                public String getName() {
-                    return template.getName();
-                }
-
-                @Override
-                public Path getPath() {
-                    return template.getPath();
-                }
-            });
-        });
-        return templateList;
+    public Optional<INode> parsedNode() {
+        return TeriumAPI.getTeriumAPI().getProvider().getNodeProvider().getNodeByName(node);
     }
 
-    @Override
-    public INode node() {
-        return new INode() {
-            @Override
-            public String getName() {
-                return node.getName();
-            }
-
-            @Override
-            public String getKey() {
-                return node.getKey();
-            }
-
-            @Override
-            public InetSocketAddress getAddress() {
-                return node.getAddress();
-            }
-
-            @Override
-            public boolean isConnected() {
-                return node.isConnected();
-            }
-
-            @Override
-            public long getUsedMemory() {
-                return node.getUsedMemory();
-            }
-
-            @Override
-            public void setUsedMemory(long usedMemory) {
-                node.setUsedMemory(usedMemory);
-            }
-
-            @Override
-            public long getMaxMemory() {
-                return node.getMaxMemory();
-            }
-
-            @Override
-            public void setMaxMemory(long maxMemory) {
-                node.setMaxMemory(maxMemory);
-            }
-
-            @Override
-            public void update() {
-                node.update();
-            }
-
-            @Override
-            public void disconnect() {
-                node.disconnect();
-            }
-
-            @Override
-            public void stop() {
-                node.stop();
-            }
-        };
-    }
-
-    @Override
-    public List<INode> fallbackNodes() {
+    public List<INode> parsedFallbackNodes() {
         List<INode> nodeList = new ArrayList<>();
         fallbackNodes.forEach(node -> {
-            nodeList.add(new INode() {
-                @Override
-                public String getName() {
-                    return node.getName();
-                }
-
-                @Override
-                public String getKey() {
-                    return node.getKey();
-                }
-
-                @Override
-                public InetSocketAddress getAddress() {
-                    return node.getAddress();
-                }
-
-                @Override
-                public boolean isConnected() {
-                    return node.isConnected();
-                }
-
-                @Override
-                public long getUsedMemory() {
-                    return node.getUsedMemory();
-                }
-
-                @Override
-                public void setUsedMemory(long usedMemory) {
-                    node.setUsedMemory(usedMemory);
-                }
-
-                @Override
-                public long getMaxMemory() {
-                    return node.getMaxMemory();
-                }
-
-                @Override
-                public void setMaxMemory(long maxMemory) {
-                    node.setMaxMemory(maxMemory);
-                }
-
-                @Override
-                public void update() {
-                    node.update();
-                }
-
-                @Override
-                public void disconnect() {
-                    node.disconnect();
-                }
-
-                @Override
-                public void stop() {
-                    node.stop();
-                }
-            });
+            nodeList.add(TeriumAPI.getTeriumAPI().getProvider().getNodeProvider().getNodeByName(node).orElseGet(null));
         });
         return nodeList;
+    }
+
+    public List<ITemplate> parsedTemplates() {
+        List<ITemplate> templateList = new ArrayList<>();
+        templates.forEach(template -> {
+            templateList.add(TeriumAPI.getTeriumAPI().getProvider().getTemplateProvider().getTemplateByName(template).orElseGet(null));
+        });
+        return templateList;
     }
 }
