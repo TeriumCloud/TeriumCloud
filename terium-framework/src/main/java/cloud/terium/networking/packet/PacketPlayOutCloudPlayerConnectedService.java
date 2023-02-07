e0ab151b0635cc8a3a43fd207a6267f4bea6b8de
Jannik H <jannikhegemann1207@gmail.com>
2023-02-07 21:53:56 +0100

    transform cloud playerpackets to new packet class structure

--- terium-framework/src/main/java/cloud/terium/networking/packet/PacketPlayOutCloudPlayerConnectedService.java
+++ terium-framework/src/main/java/cloud/terium/networking/packet/PacketPlayOutCloudPlayerConnectedService.java
@@ -1,143 +1,21 @@
 package cloud.terium.networking.packet;
 
+import cloud.terium.teriumapi.TeriumAPI;
 import cloud.terium.teriumapi.network.Packet;
-import cloud.terium.teriumapi.node.INode;
 import cloud.terium.teriumapi.player.ICloudPlayer;
 import cloud.terium.teriumapi.service.ICloudService;
-import cloud.terium.teriumapi.service.ServiceState;
-import cloud.terium.teriumapi.service.group.ICloudServiceGroup;
-import cloud.terium.teriumapi.template.ITemplate;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public record PacketPlayOutCloudPlayerConnectedService(ICloudPlayer cloudPlayer,
-                                                       ICloudService cloudService) implements Packet {
+public record PacketPlayOutCloudPlayerConnectedService(UUID cloudPlayer,
+                                                       String cloudService) implements Packet {
 
-    @Override
-    public ICloudPlayer cloudPlayer() {
-        return new ICloudPlayer() {
-            @Override
-            public String getUsername() {
-                return cloudPlayer.getUsername();
-            }
-
-            @Override
-            public UUID getUniqueId() {
-                return cloudPlayer.getUniqueId();
-            }
-
-            @Override
-            public String getAddress() {
-                return cloudPlayer.getAddress();
-            }
-
-            @Override
-            public Optional<ICloudService> getConnectedCloudService() {
-                return Optional.empty();
-            }
-        };
+    public Optional<ICloudPlayer> parsedCloudPlayer() {
+        return TeriumAPI.getTeriumAPI().getProvider().getCloudPlayerProvider().getCloudPlayer(cloudPlayer);
     }
 
-    @Override
-    public ICloudService cloudService() {
-        return new ICloudService() {
-            @Override
-            public String getServiceName() {
-                return cloudService.getServiceName();
-            }
-
-            @Override
-            public int getServiceId() {
-                return cloudService.getServiceId();
-            }
-
-            @Override
-            public int getPort() {
-                return cloudService.getPort();
-            }
-
-            @Override
-            public List<ITemplate> getTemplates() {
-                return cloudService.getTemplates();
-            }
-
-            @Override
-            public int getOnlinePlayers() {
-                return cloudService.getOnlinePlayers();
-            }
-
-            @Override
-            public void setOnlinePlayers(int onlinePlayers) {
-                cloudService.setOnlinePlayers(onlinePlayers);
-            }
-
-            @Override
-            public long getUsedMemory() {
-                return cloudService.getUsedMemory();
-            }
-
-            @Override
-            public void setUsedMemory(long usedMemory) {
-                cloudService.setUsedMemory(usedMemory);
-            }
-
-            @Override
-            public void update() {
-                cloudService.update();
-            }
-
-            @Override
-            public ICloudServiceGroup getServiceGroup() {
-                return cloudService.getServiceGroup();
-            }
-
-            @Override
-            public INode getServiceNode() {
-                return cloudService.getServiceNode();
-            }
-
-            @Override
-            public ServiceState getServiceState() {
-                return cloudService.getServiceState();
-            }
-
-            @Override
-            public void setServiceState(ServiceState serviceState) {
-                cloudService.setServiceState(serviceState);
-            }
-
-            @Override
-            public boolean isLocked() {
-                return cloudService.isLocked();
-            }
-
-            @Override
-            public void setLocked(boolean locked) {
-                cloudService.setLocked(locked);
-            }
-
-            @Override
-            public void addProperty(String name, Object property) {
-                cloudService.addProperty(name, property);
-            }
-
-            @Override
-            public void removeProperty(String name) {
-                cloudService.removeProperty(name);
-            }
-
-            @Override
-            public Object getProperty(String name) {
-                return cloudService.getProperty(name);
-            }
-
-            @Override
-            public HashMap<String, Object> getPropertyMap() {
-                return cloudService.getPropertyMap();
-            }
-        };
+    public Optional<ICloudService> parsedCloudService() {
+        return TeriumAPI.getTeriumAPI().getProvider().getServiceProvider().getCloudServiceByName(cloudService);
     }
 }
--- terium-framework/src/main/java/cloud/terium/networking/packet/PacketPlayOutCloudPlayerQuit.java
+++ terium-framework/src/main/java/cloud/terium/networking/packet/PacketPlayOutCloudPlayerQuit.java
@@ -1,36 +1,15 @@
 package cloud.terium.networking.packet;
 
+import cloud.terium.teriumapi.TeriumAPI;
 import cloud.terium.teriumapi.network.Packet;
 import cloud.terium.teriumapi.player.ICloudPlayer;
-import cloud.terium.teriumapi.service.ICloudService;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public record PacketPlayOutCloudPlayerQuit(ICloudPlayer cloudPlayer) implements Packet {
+public record PacketPlayOutCloudPlayerQuit(UUID cloudPlayer) implements Packet {
 
-    @Override
-    public ICloudPlayer cloudPlayer() {
-        return new ICloudPlayer() {
-            @Override
-            public String getUsername() {
-                return cloudPlayer.getUsername();
-            }
-
-            @Override
-            public UUID getUniqueId() {
-                return cloudPlayer.getUniqueId();
-            }
-
-            @Override
-            public String getAddress() {
-                return cloudPlayer.getAddress();
-            }
-
-            @Override
-            public Optional<ICloudService> getConnectedCloudService() {
-                return Optional.empty();
-            }
-        };
+    public Optional<ICloudPlayer> parsedCloudPlayer() {
+        return TeriumAPI.getTeriumAPI().getProvider().getCloudPlayerProvider().getCloudPlayer(cloudPlayer);
     }
 }
