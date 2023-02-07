86fbc062a3128ed581caa6c21021c743289859de
Jannik H <jannikhegemann1207@gmail.com>
2023-02-07 21:44:02 +0100

    transform node packets to new packet class structure

--- terium-framework/src/main/java/cloud/terium/networking/packet/node/PacketPlayOutNodeShutdowned.java
+++ terium-framework/src/main/java/cloud/terium/networking/packet/node/PacketPlayOutNodeShutdowned.java
@@ -1,69 +1,14 @@
 package cloud.terium.networking.packet.node;
 
+import cloud.terium.teriumapi.TeriumAPI;
 import cloud.terium.teriumapi.network.Packet;
 import cloud.terium.teriumapi.node.INode;
 
-import java.net.InetSocketAddress;
+import java.util.Optional;
 
-public record PacketPlayOutNodeShutdowned(INode node) implements Packet {
+public record PacketPlayOutNodeShutdowned(String node) implements Packet {
 
-    @Override
-    public INode node() {
-        return new INode() {
-            @Override
-            public String getName() {
-                return node.getName();
-            }
-
-            @Override
-            public String getKey() {
-                return node.getKey();
-            }
-
-            @Override
-            public InetSocketAddress getAddress() {
-                return node.getAddress();
-            }
-
-            @Override
-            public boolean isConnected() {
-                return node.isConnected();
-            }
-
-            @Override
-            public long getUsedMemory() {
-                return node.getUsedMemory();
-            }
-
-            @Override
-            public void setUsedMemory(long usedMemory) {
-                node.setUsedMemory(usedMemory);
-            }
-
-            @Override
-            public long getMaxMemory() {
-                return node.getMaxMemory();
-            }
-
-            @Override
-            public void setMaxMemory(long maxMemory) {
-                node.setMaxMemory(maxMemory);
-            }
-
-            @Override
-            public void update() {
-                node.update();
-            }
-
-            @Override
-            public void disconnect() {
-                node.disconnect();
-            }
-
-            @Override
-            public void stop() {
-                node.stop();
-            }
-        };
+    public Optional<INode> parsedNode() {
+        return TeriumAPI.getTeriumAPI().getProvider().getNodeProvider().getNodeByName(node);
     }
 }
