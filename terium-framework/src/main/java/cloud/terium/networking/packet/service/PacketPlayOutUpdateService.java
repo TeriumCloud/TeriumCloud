2b4739c9ccc8b9b63c728adb9e656836f0fa1736
Jannik H <jannikhegemann1207@gmail.com>
2023-02-08 17:46:10 +0100

    Updated PacketPlayOutUpdateService

--- terium-framework/src/main/java/cloud/terium/networking/packet/service/PacketPlayOutUpdateService.java
+++ terium-framework/src/main/java/cloud/terium/networking/packet/service/PacketPlayOutUpdateService.java
@@ -3,10 +3,12 @@ package cloud.terium.networking.packet.service;
 import cloud.terium.teriumapi.TeriumAPI;
 import cloud.terium.teriumapi.network.Packet;
 import cloud.terium.teriumapi.service.ICloudService;
+import cloud.terium.teriumapi.service.ServiceState;
 
+import java.util.HashMap;
 import java.util.Optional;
 
-public record PacketPlayOutUpdateService(String serviceName) implements Packet {
+public record PacketPlayOutUpdateService(String serviceName, int players, double memory, ServiceState serviceState, boolean locked, HashMap<String, Object> propertyCache) implements Packet {
 
     public Optional<ICloudService> parsedCloudService() {
         return TeriumAPI.getTeriumAPI().getProvider().getServiceProvider().getCloudServiceByName(serviceName);
