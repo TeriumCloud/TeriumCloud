25f35d177994391407eb787b72258cd2febd727f
Jannik H <jannikhegemann1207@gmail.com>
2023-02-22 16:40:21 +0100

    Removed DOWNLOAD and SETUP LogType in terium-api

--- terium-api/src/main/java/cloud/terium/teriumapi/console/LogType.java
+++ terium-api/src/main/java/cloud/terium/teriumapi/console/LogType.java
@@ -7,8 +7,6 @@ import java.io.Serializable;
 @Getter
 public enum LogType implements Serializable {
     NOTHING(""),
-    DOWNLOAD("\u001B[35mDOWNLOAD\u001B[0m: "),
-    SETUP("\u001B[33mSETUP\u001B[0m: "),
     SCREEN("\u001B[36mSCREEN\u001B[0m: "),
     INFO("\u001B[36mINFO\u001B[0m: "),
     WARINING("\u001B[33mWARNING\u001B[0m: "),
