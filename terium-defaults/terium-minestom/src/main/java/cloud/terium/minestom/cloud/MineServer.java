4922ae14502f1fc004cc9012f80a8fdd9af9bff1
veteex <niklasscholz1812@gmail.com>
2024-06-17 13:22:55 +0200

    Refactor MineServer to use TeriumExtension for API initialization

--- terium-defaults/terium-minestom/src/main/java/cloud/terium/minestom/cloud/MineServer.java
+++ terium-defaults/terium-minestom/src/main/java/cloud/terium/minestom/cloud/MineServer.java
@@ -1,57 +1,64 @@
 package cloud.terium.minestom.cloud;
 
 import cloud.terium.extension.TeriumExtension;
-import cloud.terium.teriumapi.TeriumAPI;
 import net.kyori.adventure.text.Component;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.AsyncPlayerPreLoginEvent;
-import net.minestom.server.event.player.PlayerCommandEvent;
 import net.minestom.server.event.player.PlayerDisconnectEvent;
-import net.minestom.server.event.player.PlayerLoginEvent;
 import net.minestom.server.extras.PlacementRules;
 import net.minestom.server.extras.bungee.BungeeCordProxy;
 import net.minestom.server.extras.optifine.OptifineSupport;
 import net.minestom.server.extras.velocity.VelocityProxy;
-import net.minestom.server.instance.InstanceContainer;
-import net.minestom.server.instance.InstanceManager;
 
 public class MineServer extends TeriumExtension {
+
+    private static TeriumExtension extension;
 
     public static void main(String[] args) {
         MinecraftServer minecraftServer = MinecraftServer.init();
         OptifineSupport.enable();
         PlacementRules.init();
 
+        extension = new TeriumExtension() {
+            @Override
+            public void executeCommand(String command) {
+                MinecraftServer.getSchedulerManager().buildTask(() -> MinecraftServer.getCommandManager().getDispatcher().execute(MinecraftServer.getCommandManager().getConsoleSender(), command));
+            }
+        };
+
         /*
             This code snipe is important to paste this in every Minestom Server project
 
             START
          */
-        if(TeriumAPI.getTeriumAPI().getProvider().getThisService().getServiceGroup().getVersion().contains("bungeecord"))
+        if(extension.getProvider().getThisService().getServiceGroup().getVersion().contains("bungeecord")) {
             BungeeCordProxy.enable();
+        }
 
-        if(TeriumAPI.getTeriumAPI().getProvider().getThisService().getServiceGroup().getVersion().contains("velocity"))
-            VelocityProxy.enable("SECRET FROM COPIED FORWARDING.SECRET OF YOUR VELOCITY SERVICE");
+        if(extension.getProvider().getThisService().getServiceGroup().getVersion().contains("velocity")) {
+            VelocityProxy.enable("PASTE YOUR VELOCITY SECRET KEY HERE");
+        }
+
         GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
         globalEventHandler.addEventCallback(AsyncPlayerPreLoginEvent.class, event -> {
-            if (TeriumAPI.getTeriumAPI().getProvider().getThisService().isLocked() && !event.getPlayer().hasPermission("terium.locked.join"))
+            if (extension.getProvider().getThisService().isLocked() && !event.getPlayer().hasPermission("terium.locked.join"))
                 event.getPlayer().kick(Component.text("§cThis service is locked."));
-            TeriumAPI.getTeriumAPI().getProvider().getThisService().setOnlinePlayers(MinecraftServer.getConnectionManager().getOnlinePlayers().size());
-            TeriumAPI.getTeriumAPI().getProvider().getThisService().update();
-            TeriumAPI.getTeriumAPI().getProvider().getCloudPlayerProvider().getCloudPlayer(event.getPlayer().getUuid()).ifPresent(cloudPlayer -> {
+            extension.getProvider().getThisService().setOnlinePlayers(MinecraftServer.getConnectionManager().getOnlinePlayers().size());
+            extension.getProvider().getThisService().update();
+            extension.getProvider().getCloudPlayerProvider().getCloudPlayer(event.getPlayer().getUuid()).ifPresent(cloudPlayer -> {
                 cloudPlayer.updateUsername(event.getPlayer().getUsername());
-                cloudPlayer.updateConnectedService(TeriumAPI.getTeriumAPI().getProvider().getThisService());
+                cloudPlayer.updateConnectedService(extension.getProvider().getThisService());
                 cloudPlayer.update();
             });
         });
 
         globalEventHandler.addEventCallback(PlayerDisconnectEvent.class, event -> {
-            TeriumAPI.getTeriumAPI().getProvider().getThisService().setOnlinePlayers(MinecraftServer.getConnectionManager().getOnlinePlayers().size() - 1);
-            TeriumAPI.getTeriumAPI().getProvider().getThisService().update();
+            extension.getProvider().getThisService().setOnlinePlayers(MinecraftServer.getConnectionManager().getOnlinePlayers().size() - 1);
+            extension.getProvider().getThisService().update();
         });
 
-        minecraftServer.start(TeriumAPI.getTeriumAPI().getProvider().getThisNode().getAddress().getAddress().getHostAddress(), TeriumAPI.getTeriumAPI().getProvider().getThisService().getPort());
+        minecraftServer.start(extension.getProvider().getThisNode().getAddress().getAddress().getHostAddress(), extension.getProvider().getThisService().getPort());
         /*
             END
          */
