2aa8006556c155b090567bc170fa237b21f1b92e
Jannik H <jannikhegemann1207@gmail.com>
2024-06-19 16:49:24 +0200

    updated TeriumMinestomExtension.java

--- terium-minestom-extension/src/main/java/cloud/terium/minestom/extension/TeriumMinestomExtension.java
+++ terium-minestom-extension/src/main/java/cloud/terium/minestom/extension/TeriumMinestomExtension.java
@@ -13,6 +13,9 @@ import net.minestom.server.extras.bungee.BungeeCordProxy;
 import net.minestom.server.extras.velocity.VelocityProxy;
 import net.minestom.server.utils.time.TimeUnit;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TeriumMinestomExtension {
 
     private final TeriumExtension extension;
@@ -22,50 +25,44 @@ public class TeriumMinestomExtension {
      * ! DO NOT EXECUTE IN YOUR CODE 'minecraftServer#start()' ITS IMPLEMENTED IN THE TERIUM-EXTENSION !
      */
     public TeriumMinestomExtension(MinecraftServer minecraftServer, Proxy proxy) {
-        System.out.println(0);
         extension = new TeriumExtension() {
             @Override
             public void executeCommand(String command) {
                 MinecraftServer.getSchedulerManager().buildTask(() -> MinecraftServer.getCommandManager().getDispatcher().execute(MinecraftServer.getCommandManager().getConsoleSender(), command));
             }
         };
-        System.out.println(1);
-        MinecraftServer.getSchedulerManager().buildTask(() -> {
-            System.out.println(2);
-            extension.successfulStart();
 
-            System.out.println(3);
-            GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
-            globalEventHandler.addEventCallback(AsyncPlayerPreLoginEvent.class, event -> {
-                if (extension.getProvider().getThisService().isLocked() && !event.getPlayer().hasPermission("terium.locked.join"))
-                    event.getPlayer().kick(Component.text("§cThis service is locked."));
-                extension.getProvider().getThisService().setOnlinePlayers(MinecraftServer.getConnectionManager().getOnlinePlayers().size());
-                extension.getProvider().getThisService().update();
-                extension.getProvider().getCloudPlayerProvider().getCloudPlayer(event.getPlayer().getUuid()).ifPresent(cloudPlayer -> {
-                    cloudPlayer.updateUsername(event.getPlayer().getUsername());
-                    cloudPlayer.updateConnectedService(extension.getProvider().getThisService());
-                    cloudPlayer.update();
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                extension.successfulStart();
+
+                GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+                globalEventHandler.addEventCallback(AsyncPlayerPreLoginEvent.class, event -> {
+                    if (extension.getProvider().getThisService().isLocked() && !event.getPlayer().hasPermission("terium.locked.join"))
+                        event.getPlayer().kick(Component.text("§cThis service is locked."));
+                    extension.getProvider().getThisService().setOnlinePlayers(MinecraftServer.getConnectionManager().getOnlinePlayers().size());
+                    extension.getProvider().getThisService().update();
+                    extension.getProvider().getCloudPlayerProvider().getCloudPlayer(event.getPlayer().getUuid()).ifPresent(cloudPlayer -> {
+                        cloudPlayer.updateUsername(event.getPlayer().getUsername());
+                        cloudPlayer.updateConnectedService(extension.getProvider().getThisService());
+                        cloudPlayer.update();
+                    });
                 });
-            });
 
-            System.out.println(4);
+                globalEventHandler.addEventCallback(PlayerDisconnectEvent.class, event -> {
+                    extension.getProvider().getThisService().setOnlinePlayers(MinecraftServer.getConnectionManager().getOnlinePlayers().size() - 1);
+                    extension.getProvider().getThisService().update();
+                });
 
-            globalEventHandler.addEventCallback(PlayerDisconnectEvent.class, event -> {
-                extension.getProvider().getThisService().setOnlinePlayers(MinecraftServer.getConnectionManager().getOnlinePlayers().size() - 1);
-                extension.getProvider().getThisService().update();
-            });
+                if(proxy instanceof Velocity velocity)
+                    VelocityProxy.enable(velocity.getForwardingSecret());
+                else if(proxy instanceof BungeeCord)
+                    BungeeCordProxy.enable();
+                else System.out.println("No vaild Proxy-Instance found!");
 
-            System.out.println(5);
-
-            if(proxy instanceof Velocity velocity)
-                VelocityProxy.enable(velocity.getForwardingSecret());
-            else if(proxy instanceof BungeeCord)
-                BungeeCordProxy.enable();
-            else System.out.println("No vaild Proxy-Instance found!");
-
-            System.out.println(6);
-
-            minecraftServer.start(extension.getProvider().getThisNode().getAddress().getAddress().getHostAddress(), extension.getProvider().getThisService().getPort());
-        }).delay(2, TimeUnit.SECOND).schedule();
+                minecraftServer.start(extension.getProvider().getThisNode().getAddress().getAddress().getHostAddress(), extension.getProvider().getThisService().getPort());
+            }
+        }, 0, 2000);
     }
 }
